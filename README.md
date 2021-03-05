92a6daa323a333b89e76bc0b929b1095f1f6bddc
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2021-03-05 17:08:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a small PHP project designed to show how to use JWTs for authorization, with minimal usage of third-party libraries.
---
+++ public/authenticate.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Firebase\JWT\JWT;
+
+require_once('../vendor/autoload.php');
+
+// Validate the credentials against a database, or other data store.
+// ...
+// For the purposes of this example, we'll assume that they're valid
+$hasValidCredentials = true;
+
+if ($hasValidCredentials) {
+    $secretKey  = 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=';
+    $tokenId    = base64_encode(random_bytes(16));
+    $issuedAt   = new DateTimeImmutable();
+    $expire     = $issuedAt->modify('+6 minutes')->getTimestamp();      // Add 60 seconds
+    $serverName = "your.domain.name";
+    $username   = "username";                                           // Retrieved from filtered POST data
+
+    // Create the token as an array
+    $data = [
+        'iat'  => $issuedAt,         // Issued at: time when the token was generated
+        'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
+        'iss'  => $serverName,       // Issuer
+        'nbf'  => $issuedAt,         // Not before
+        'exp'  => $expire,           // Expire
+        'data' => [                  // Data related to the signer user
+            'userName' => $username, // User name
+        ]
+    ];
+
+    // Encode the array to a JWT string.
+    echo JWT::encode(
+        $data,      //Data to be encoded in the JWT
+        $secretKey, // The signing key
+        'HS512'     // Algorithm used to sign the token, see https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-3
+    );
+}
